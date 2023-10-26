36d6cc36eb5c327b5111b9df35a7c9d51f99a09e
MMGGHD <tldmseogus@naver.com>
2023-10-26 09:37:49 +0900

    [김대현] 231026

--- lib/ui/screens/category/category_screen.dart
+++ lib/ui/screens/category/category_screen.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'widget/category_product.dart';
+
 class CategoryScreen extends StatelessWidget {
   const CategoryScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("categoryScreen"),
-      ),
+      body: CategoryProducts(),
     );
   }
 }
